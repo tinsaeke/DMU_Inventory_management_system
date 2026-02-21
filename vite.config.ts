c4a1ad185d1682fa1c2ab73701c20ae3a0846072
tinsaekassie <tinsaeke31@gmail.com>
2026-02-21 10:13:46 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/DMU_Inventory_management_system/',
   server: {
     host: "::",
     port: 8080,
